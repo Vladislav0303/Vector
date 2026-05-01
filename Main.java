a036b2117cc808cd83fca9f0ebaabc292cfd6545
Vladislav0303 <vladshulyak003@gmail.com>
2026-05-01 10:45:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
         MyVector vector = new MyVector();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < vector.mass.length; i++) {
+            while (i < vector.mass.length / 2) {
+                System.out.println("Введіть значення яке хочете записати в масив: ");
+                int el = sc.nextInt();
+                sc.nextLine();
+                vector.mass[i] = el;
+                i++;
+            }
+        }
+        vector.get();
+        System.out.println("Введіть число яке хочете додати в кінець масиву: ");
+        int el = sc.nextInt();
+        vector.Add_Element_In_End(el);
+        vector.get();
+        sc.nextLine();
+        System.out.println("Введіть число який хочете додати в середину масиву: ");
+        int el1 = sc.nextInt();
+        vector.Add_Element_In_Between(el1);
+        vector.get();
+        sc.nextLine();
+        System.out.println("Введіть число який хочете додати в початок масиву: ");
+        int el2 = sc.nextInt();
+        vector.Add_Element_In_Start(el2);
+        vector.get();
+        sc.nextLine();
+        vector.Add_Element();
+        System.out.println("Введіть елемент який хочете видалити за індексом: ");
+        int el3 = sc.nextInt();
+        vector.Delete_Element(el3);
+        vector.get();
+        vector.capasity();
+        sc.nextLine();
+        System.out.println("Хочете видалити весь масив?: ");
+        if (sc.nextLine().equals("Так")) {
+            vector.Clear_Mass();
+        }
+        vector.get();
     }
 }
--- MyVector.java
+++ MyVector.java
@@ -10,43 +10,6 @@ public class MyVector {
         System.out.println("Введіть розмір масиву: ");
         int size = sc.nextInt();
         mass = new int[size * 2];
-        for (int i = 0; i < mass.length; i++) {
-            while (i < size) {
-                System.out.println("Введіть значення яке хочете записати в масив: ");
-                int el = sc.nextInt();
-                sc.nextLine();
-                mass[i] = el;
-                i++;
-            }
-    }
-        get();
-        System.out.println("Введіть число яке хочете додати в кінець масиву: ");
-        int el = sc.nextInt();
-        Add_Element_In_End(el);
-        get();
-        sc.nextLine();
-        System.out.println("Введіть число який хочете додати в середину масиву: ");
-        int el1 = sc.nextInt();
-        Add_Element_In_Between(el1);
-        get();
-        sc.nextLine();
-        System.out.println("Введіть число який хочете додати в початок масиву: ");
-        int el2 = sc.nextInt();
-        Add_Element_In_Start(el2);
-        get();
-        sc.nextLine();
-        Add_Element();
-        System.out.println("Введіть елемент який хочете видалити за індексом: ");
-        int el3 = sc.nextInt();
-        Delete_Element(el3);
-        get();
-        capasity();
-        sc.nextLine();
-        System.out.println("Хочете видалити весь масив?: ");
-        if (sc.nextLine().equals("Так")) {
-            Clear_Mass();
-        }
-        get();
     }
     public void Add_Element_In_End(int el) {
         for (int i = 0; i < mass.length; i++) {
